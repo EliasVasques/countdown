42754d2cca2c40ff7c08ca51d1a7145df4b7b4ed
EliasVasques <eliasvasques74@gmail.com>
2022-01-24 09:31:28 -0300

    finalizando countdown

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
+function updateCountDown () {
+    const eventDate = new Date(2022, 0, 25, 14, 0, 0, 0);
+    const now = new Date();
+    let gap = eventDate - now;
 
-const eventDate = new Date(2022, 0, 25, 14, 0, 0, 0);
-                        //      jan    2 pm
-const now = new Date();
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
 
-let diffDates = eventDate - now;
-console.log((eventDate - now) / 86400000);
+    let textDay = Math.floor(gap / day);
+    let textHour = Math.floor((gap % day) / hour);
+    let textMinute = Math.floor((gap % hour) / minute);
+    let textSecond = Math.floor((gap % minute) / second);
 
-
-let secondsInDay = 86400;
-let secondsInHour = 3600;
-let secondsInMinute = 60;
-let secondsInSecond = 1;
-
-//let totalSeconds = Date.
-
-
-
-let n = Math.trunc(5.2)
-console.log(n)
-let day = document.querySelector('#day .value');
-let diffDay = Math.trunc(diffDates / 86400000);
-            // milisegundos num dia
-diffDates = diffDay % (diffDates / 86400000);
-console.log(diffDates)
-day.innerHTML = diffDay
-
-let hour = document.querySelector('#hour .value');
-hour.innerHTML = eventDate.getHours() - now.getHours();
-
-let minute = document.querySelector('#minute .value');
-minute.innerHTML = eventDate.getMinutes() - now.getMinutes();
-
-let second = document.querySelector('#second .value');
-second.innerHTML = eventDate.getSeconds() - now.getSeconds();
-
-console.log(now.getFullYear())
+    document.querySelector('#day .value').innerHTML = textDay;
+    document.querySelector('#hour .value').innerHTML = textHour;
+    document.querySelector('#minute .value').innerHTML = textMinute;
+    document.querySelector('#second .value').innerHTML = textSecond;
+}
+setInterval(updateCountDown, 1000);
